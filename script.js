dc05e898081df29df825e7c3ec15f1dc1ddb95dd
Mykola Sliepchenko <icmykola.sliepchenko@americanwell.com>
2023-07-08 19:01:14 +0200

    Change complexity

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 (() => {
-    const MAX_LEVEL = 25;
+    const MAX_LEVEL = 50;
 
     const containerElement = document.querySelector('.container');
 
@@ -11,7 +11,7 @@
 
     const generate = () => {
         const randomA = Math.ceil(Math.random() * 1000);
-        const randomB = Math.ceil(Math.random() * 50);
+        const randomB = Math.ceil(Math.random() * 9) + 1;
 
         const rowElement = document.createElement('div');
         rowElement.innerHTML = `
@@ -39,7 +39,7 @@
             // handle correct and incorrect answers
             if (Math.floor(randomA / randomB) === Number(resultElement.value)) {
                 rowElement.style.backgroundColor = '#d9ffd1';
-                score += 4;
+                score += 2;
             } else {
                 rowElement.style.backgroundColor = '#ffb5b5';
             }
